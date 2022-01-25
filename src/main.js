2516a8ecee1435df923430072fa8094b98c08005
buptmint <56798262+buptmint@users.noreply.github.com>
2022-01-25 16:29:55 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,3 +7,12 @@ import './index.css'
 const app = createApp(App);
 app.use(ElementPlus)
 app.mount('#app')
+
+/*
+各位面试官们，中午好
+我很荣幸参加此次SAP公司上海地区Associate Software Developer职位的面试
+我的名字是张昊东，是2022届即将毕业于北京邮电大学自动化学院专业的应届毕业生，在中国以高水平的计算机学科排名著称
+我在校期间主要的课程为计算机和机械自动化的相关专业课程，我的求职方向偏向于前端交互相关的开发工作
+现在的工作技术栈包括：JavaScript，H5开发，Vue,React,Webpack,Vite以及其他
+我曾参加过两段与软件研发相关的实习经历，第一段是在一家人力资源SaaS云计算领域的公司叫北森云计算公司，从事React技术栈相关的开发，公司内部使用以React为基础的低代码平台开发了人才大数据搜索平台项目功能
+*/
